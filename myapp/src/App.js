8b69fcc3cefebc85c1792b4bef9571d87fb9b855
Alfredo G <agomez@g.hmc.edu>
2020-06-08 13:43:06 -0700

    Parameters stored in main App

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,10 +5,36 @@ import Sidebar from "./comps/sidebar/sidebar"
 import Page from "./comps/page/page"
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { iter: 0, numTopics: 25 };
+
+    this.updateIter = this.updateIter.bind(this);
+    this.updateNumTopics = this.updateNumTopics.bind(this);
+
+  };
+
+  updateIter() {
+    this.setState({
+      iter: this.state.iter + 1
+    });
+    console.log("Clicked");
+  }
+
+  updateNumTopics(val) {
+    this.setState({
+      numTopics: val
+    });
+    console.log("Topics: " + this.state.numTopics);
+  }
+
   render() {
+
     return (
       <div className="App">
-        <Form />
+        <Form iter={this.state.iter} numTopics = {this.state.numTopics} onClick={this.updateIter} onChange = {this.updateNumTopics}/>
         <Sidebar />
         <Page />
       </div>
