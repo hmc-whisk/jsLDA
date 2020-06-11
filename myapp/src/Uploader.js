60fce70862f2f49c1b2f950eed627456afdcc01d
Tats777 <54814305+Tats777@users.noreply.github.com>
2020-06-11 11:59:56 +0800

    Files added to help merge later
    Currently, visuals for all the files have not yet been updated to fit react style. We currently have a problem in uploading, where the getStopListUpload function called in queueLoad is returning null instead of the output we need. Thus, we are unable to load the documents required and the site, and are unable to test the other portions of the code.

--- myapp/src/Uploader.js
+++ myapp/src/Uploader.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React from 'react';
 
-class Uploader extends React.Component {
 
-    render() {
-        return (
-            <div >
-                <div class="upload">
-                    <form onSubmit="event.preventDefault(); queueLoad();">
-                        <div>Use a different collection:</div>
-                        <div>Documents <input id="docs-file-input" type="file" onchange="onDocumentFileChange(this)"
-                            size="10" /></div>
-                        <div>Stoplist <input id="stops-file-input" type="file" onchange="onStopwordFileChange(this)"
-                            size="10" /></div>
-                        <div><button id="load-inputs">Upload</button></div>
-                    </form>
-                </div>
-
+/**
+ * @summary An upload component
+ * @description A function component of a file upload interface
+ * with a document and stopword file selector as well
+ * as a upload button
+ * 
+ * @prop { onDocumentFileChange(event) }
+ *      - The function to be called when
+ *        the document file is changed
+ * @prop { onStopwordFileChange(event) }
+ *      - The function to be called when
+ *        the stopword file is changed
+ * @prop { onFileUpload() }
+ *      - The function to be called when
+ *        the upload button is pressed
+ * 
+ */
+export function Uploader(props) {
+    return(
+      <div className="upload">
+        <form>
+            <div>Use a different collection:</div>
+            <div>
+                <label htmlFor="docs-file-input">Documents</label> 
+                <input id="docs-file-input" type="file" 
+                onChange={props.onDocumentFileChange(this)} size="10"/>
             </div>
-        );
-    }
-
+            <div>
+                <label htmlFor="stops-file-input" >Stoplist</label>  
+                <input id="stops-file-input" type="file" 
+                onChange={props.onStopwordFileChange(this)} size="10"/>
+            </div>
+            <div>
+                <button id="load-inputs" onClick={(event) => {event.preventDefault(); props.onFileUpload();} }
+                type='button'>
+                Upload
+                </button>
+            </div>
+        </form>
+      </div>
+    )
 }
 
-export default Uploader;
+export default Uploader
