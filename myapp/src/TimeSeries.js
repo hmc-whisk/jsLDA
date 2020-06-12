7541e2233cddb518fe2bd561f133853dccadf35e
theobayard <theobayarddevolo@gmail.com>
2020-06-12 14:36:11 -0500

    Cleaned some code

--- myapp/src/TimeSeries.js
+++ myapp/src/TimeSeries.js
@@ -20,12 +20,22 @@ class TimeSeries extends Component {
         tsPage.select("svg").remove();
         this.state.topicTimeGroups = [];
         
-        var tsSVG = tsPage.append("svg").attr("height", this.state.timeSeriesHeight * this.props.numTopics).attr("width", this.state.timeSeriesWidth);
-    
+        var tsSVG = tsPage
+                        .append("svg")
+                        .attr("height", this.state.timeSeriesHeight * this.props.numTopics)
+                        .attr("width", this.state.timeSeriesWidth);
+        
+        let temp_topicTimeGroups = this.state.topicTimeGroups.slice();
         for (var topic = 0; topic < this.props.numTopics; topic++) {
-        this.state.topicTimeGroups.push(tsSVG.append("g").attr("transform", "translate(0," + (this.state.timeSeriesHeight * topic) + ")"));
-        this.state.topicTimeGroups[topic].append("path").style("fill", "#ccc").style("stroke", "#fff");
-        this.state.topicTimeGroups[topic].append("text").attr("y", 40);
+            temp_topicTimeGroups
+                .push(tsSVG
+                    .append("g")
+                    .attr("transform", "translate(0," + (this.state.timeSeriesHeight * topic) + ")"));
+            temp_topicTimeGroups[topic]
+                .append("path")
+                .style("fill", "#ccc")
+                .style("stroke", "#fff");
+            temp_topicTimeGroups[topic].append("text").attr("y", 40);
         }
     }
 
@@ -34,19 +44,36 @@ class TimeSeries extends Component {
         var tsPage = d3.select("#ts-page");
     
         for (var topic = 0; topic < this.props.numTopics; topic++) {
-        var topicProportions = this.props.documents.map(function (d) { return {date: d.date, p: d.topicCounts[topic] / d.tokens.length}; })
-        var topicMeans = d3.nest().key(function (d) {return d.date; }).rollup(function (d) {return d3.mean(d, function (x) {return x.p}); }).entries(topicProportions);
-    
-        var xScale = d3.scaleLinear().domain([0, topicMeans.length]).range([0, this.state.timeSeriesWidth]);
-        var yScale = d3.scaleLinear().domain([0, 0.2]).range([this.state.timeSeriesHeight, 0]);
-        var area = d3.area()
-        .x(function (d, i) { return xScale(i); })
-        .y1(function (d) { return yScale(d.value); })
-        .y0(yScale(0));
+            var topicProportions = this.props.documents
+                .map(function (d) { 
+                    return {date: d.date, p: d.topicCounts[topic] / d.tokens.length}; 
+                });
+            var topicMeans = d3
+                .nest()
+                .key(function (d) {return d.date; })
+                .rollup(function (d) {return d3
+                    .mean(d, function (x) {return x.p}); })
+                .entries(topicProportions);
         
-        if (this.state.topicTimeGroups[topic]) {
-        this.state.topicTimeGroups[topic].select("path").attr("d", area(topicMeans));
-        this.state.topicTimeGroups[topic].select("text").text(this.state.topNWords(this.props.topicWordCounts[topic], 3))}
+            var xScale = d3.scaleLinear()
+                .domain([0, topicMeans.length])
+                .range([0, this.state.timeSeriesWidth]);
+            var yScale = d3
+                .scaleLinear()
+                .domain([0, 0.2])
+                .range([this.state.timeSeriesHeight, 0]);
+            var area = d3.area()
+                .x(function (d, i) { return xScale(i); })
+                .y1(function (d) { return yScale(d.value); })
+                .y0(yScale(0));
+            
+            if (this.state.topicTimeGroups[topic]) {
+                this.state.topicTimeGroups[topic]
+                    .select("path")
+                    .attr("d", area(topicMeans));
+            this.state.topicTimeGroups[topic]
+                .select("text")
+                .text(this.state.topNWords(this.props.topicWordCounts[topic], 3))}
         } 
     }
   
