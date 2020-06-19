eb7a3e8c1a92812d26fd956b38944b75f855179a
theobayard <theobayarddevolo@gmail.com>
2020-06-19 15:34:34 -0500

    Changed Directory Structure

--- myapp/src/Components/Pages/TopicDoc.js
+++ myapp/src/Components/Pages/TopicDoc.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'; 
 import * as d3 from 'd3';
-import Uploader from '../Uploader';
+import Uploader from './Uploader';
 
 /////@TODO rename this file to TopicDoc.js?/////
 /*
