523810b5d2d0bd34bbd2467c4b2c303a09c305d4
theobayard <theobayarddevolo@gmail.com>
2020-06-23 12:34:50 -0500

    added "_private" notation

--- myapp/src/LDAModel/LDAModel.js
+++ myapp/src/LDAModel/LDAModel.js
@@ -1,58 +1,58 @@
 class LDAModel {
     constructor() {
         // Needed by reset & parseline
-        vocabularySize = 0;
+        _vocabularySize = 0;
 
         // Constants for calculating topic correlation. A doc with 5% or more tokens in a topic is "about" that topic.
-        correlationMinTokens = 2;
-        correlationMinProportion = 0.05;
+        _correlationMinTokens = 2;
+        _correlationMinProportion = 0.05;
 
 
         // needed by reset & parseline, changeNumTopics
-        vocabularyCounts = {};
+        _vocabularyCounts = {};
 
         //needed by reset
-        sortVocabByTopic = false;
-        this.specificityScale = d3.scaleLinear().domain([0,1]).range(["#ffffff", "#99d8c9"]);
+        _sortVocabByTopic = false;
+        _this.specificityScale = d3.scaleLinear().domain([0,1]).range(["#ffffff", "#99d8c9"]);
 
-        wordPattern = XRegExp("\\p{L}[\\p{L}\\p{P}]*\\p{L}", "g");
+        _wordPattern = XRegExp("\\p{L}[\\p{L}\\p{P}]*\\p{L}", "g");
 
         // Topic model parameters
 
         // needed by reset & sortTopicWords, changeNumTopics
-        this.numTopics = 0; // run findNumTopics upon mount
+        this._numTopics = 0; // run findNumTopics upon mount
 
         // required by reset & ready & parseline
-        this.stopwords = {};
+        this._stopwords = {};
 
         // required by reset, changeNumTopics
-        this.completeSweeps = 0;
-        this.requestedSweeps = 0;
+        this._completeSweeps = 0;
+        this._requestedSweeps = 0;
 
         // in reset, changeNumTopics
 
-        selectedTopic = 0;
+        _selectedTopic = 0;
 
         // Needed by reset & parseline & sortTopicWords, changeNumTopics
-        this.wordTopicCounts = {};
+        this._wordTopicCounts = {};
 
         // Needed by reset & sortTopicWords, changeNumTopics
-        this.topicWordCounts = [];
+        this._topicWordCounts = [];
 
         // Needed by reset & parseline, changeNumTopics
-        this.tokensPerTopic = []; // set to zeros(numTopics)
+        this._tokensPerTopic = []; // set to zeros(numTopics)
 
         // in reset, changeNumTopics
-        this.topicWeights = []; // set to zeros(numTopics)
+        this._topicWeights = []; // set to zeros(numTopics)
 
         // Array of dictionaries with keys 
         // {"originalOrder", "id", "date", "originalText", "tokens", "topicCounts"}
         // used by reset & parseline, changeNumTopics
-        this.documents = [];
+        this._documents = [];
 
-        this.timer = 0; // used in sweep
-        this.documentTopicSmoothing = 0.1; // (used by sweep)
-        this.topicWordSmoothing = 0.01; // (used by sweep)
+        this._timer = 0; // used in sweep
+        this._documentTopicSmoothing = 0.1; // (used by sweep)
+        this._topicWordSmoothing = 0.01; // (used by sweep)
     }
 
     _byCountDescending(a,b) { return b.count - a.count; };
