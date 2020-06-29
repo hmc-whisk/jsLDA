f5d41bbc654f37b58db5245b04447eab5450f128
Alfredo G <agomez@g.hmc.edu>
2020-06-29 09:56:35 -0700

    Changed bindings for consistency

--- myapp/src/Components/Header/TopBar.js
+++ myapp/src/Components/Header/TopBar.js
@@ -6,26 +6,23 @@ class TopBar extends React.Component {
         super(props);
 
         this.state = {sliderValue:25};
-        this.handleClick = this.handleClick.bind(this)
-        this.updateNumDisplay = this.updateNumDisplay.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+
     }
 
-    handleClick() {
+    handleClick = () => {
         this.props.onClick();
     }
 
-    updateNumDisplay(event) {
+    updateNumDisplay = (event) => {
         if(!event.target.value) {return}
         let val = event.target.value;
         this.setState({
             sliderValue: val
         });
     }
-    handleChange(e) {
+    handleChange = (event) => {
         // Either value number or 1 (avoids NaN)
-        const val = e.target.value || 1;
+        const val = event.target.value || 1;
 
         this.props.onChange(val);
     }
@@ -38,12 +35,13 @@ class TopBar extends React.Component {
                 <input onChange = {this.handleChange} value = {this.props.sweepParameter} type="text" pattern="\d*" maxlength="5" size = "5"/>
                 <button id="sweep" onClick={this.handleClick} >Run {this.props.sweepParameter} iterations</button> 
                 <button id="stopSweep" onClick={() => this.props.stopButtonClick()}>Stop</button>
-                Iterations:<span id="iters"> {this.props.iter} </span>
+                Iterations:<span id="iters" > {this.props.iter} </span>
                 <NumTopicSlider 
                     onChange={this.updateNumDisplay} 
                     sliderValue = {this.state.sliderValue} 
                     updateNumTopics={this.props.updateNumTopics}
                     onInput={(event) => this.updateNumDisplay(event)}
+                    modelIsRunning = {this.props.modelIsRunning}
                     /> 
             </div>
         );
