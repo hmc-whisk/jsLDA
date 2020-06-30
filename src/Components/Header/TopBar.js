f948bfeb44e5d9b4e9676810b6a79f3a36ae0c21
Alfredo G <agomez@g.hmc.edu>
2020-06-30 11:42:10 -0700

    Rework Inpur as Form Element

--- src/Components/Header/TopBar.js
+++ src/Components/Header/TopBar.js
@@ -5,16 +5,22 @@ class TopBar extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {sliderValue:25};
+        this.state = { sliderValue: 25 };
 
     }
 
-    handleClick = () => {
+    handleSubmit = (event) => {
         this.props.onClick();
+        event.preventDefault();
+    }
+
+    handleClick = (event) => {
+        this.props.stopButtonClick();
+        event.preventDefault();
     }
 
     updateNumDisplay = (event) => {
-        if(!event.target.value) {return}
+        if (!event.target.value) { return }
         let val = event.target.value;
         this.setState({
             sliderValue: val
@@ -22,7 +28,12 @@ class TopBar extends React.Component {
     }
     handleChange = (event) => {
         // Either value number or 1 (avoids NaN)
-        const val = event.target.value || 1;
+        const val = event.target.value||0;
+        
+        // if (val === NaN || val === 0) {
+        //     val = 1;
+        //     console.log("val is now " + val)
+        // }
 
         this.props.onChange(val);
     }
@@ -31,19 +42,28 @@ class TopBar extends React.Component {
     render() {
         return (
             <div id="form" className="top">
-                Enter Number of Iterations: &nbsp;
-                <input onChange = {this.handleChange} value = {this.props.sweepParameter} type="text" pattern="\d*" maxlength="5" size = "5"/>
-                <button id="sweep" onClick={this.handleClick} >Run {this.props.sweepParameter} iterations</button> 
-                <button id="stopSweep" onClick={() => this.props.stopButtonClick()} disabled = {!this.props.modelIsRunning}>Stop</button>
+                <form onSubmit = {this.handleSubmit}>
+                    <label for="number">Enter Number of Iterations: &nbsp;</label>
+
+                    <input onChange = {this.handleChange} id="number" type="number" placeholder="50" min="1" max="100000" id="number" required></input>
+
+                    <button type = "submit" id="sweep" >Run {this.props.sweepParameter} iterations</button>
+
+                    <button id="stopSweep" onClick={this.handleClick} disabled={!this.props.modelIsRunning}>Stop</button>
+
                 Iterations:<span id="iters" > {this.props.iter} </span>
-                <NumTopicSlider 
-                    onChange={this.updateNumDisplay} 
-                    sliderValue = {this.state.sliderValue} 
-                    updateNumTopics={this.props.updateNumTopics}
-                    onInput={(event) => this.updateNumDisplay(event)}
-                    modelIsRunning = {this.props.modelIsRunning}
-                    /> 
+
+                    <NumTopicSlider
+                        onChange={this.updateNumDisplay}
+                        sliderValue={this.state.sliderValue}
+                        updateNumTopics={this.props.updateNumTopics}
+                        onInput={(event) => this.updateNumDisplay(event)}
+                        modelIsRunning={this.props.modelIsRunning}
+                    />
+                </form>
             </div>
+
+
         );
     }
 
