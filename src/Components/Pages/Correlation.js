a5cb2c7e1c8ea0db33d260ecfed5441d70227c70
Alfredo G <agomez@g.hmc.edu>
2020-07-07 08:33:26 -0700

    Remove Console Logs

--- src/Components/Pages/Correlation.js
+++ src/Components/Pages/Correlation.js
@@ -95,8 +95,7 @@ class Correlation extends Component {
         .attr("cy", function(link) { return topicScale(link.target); })
         .attr("r", function (link) { return radiusScale(Math.abs(link.value)); })
         .style("fill", function (link) { return link.value > 0.0 ? "#88f" : "#f88"; })
-        .on("mouseover", function (link, event) {
-          console.log(event);
+        .on("mouseover", function (link) {
           var tooltip = d3.select("#tooltip");
           var tooltipX = this.getBoundingClientRect().x + window.scrollX;
           var tooltipY = this.getBoundingClientRect().y + window.scrollY ;
