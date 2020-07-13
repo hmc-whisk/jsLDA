42659f932d38004b20a8965286b9244317d42c2a
theobayard <theobayarddevolo@gmail.com>
2020-07-13 17:45:58 -0500

    Added yelp data

--- src/Components/Pages/Uploader.js
+++ src/Components/Pages/Uploader.js
@@ -28,6 +28,7 @@ export function Uploader(props) {
                     <select id="defaultDoc" onChange = {(event) => props.onDefaultDocChange(event)} value = {props.docName}>
                         <option value="Movie Plots">Movie Plots</option>
                         <option value="State Of The Union">State Of The Union</option>
+                        <option value="Yelp Reviews">Yelp Reviews</option>
                     </select>
                     <input type="submit" value="Reset" disabled={props.modelIsRunning} />
                 </form>
