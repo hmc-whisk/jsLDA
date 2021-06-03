0cb17b95e28ce25544a947d20c83f955552001a0
Mia Celeste <mia@miaceleste.dev>
2021-06-03 00:30:14 -0700

    Typed and reworked Header/NavBar

--- src/Components/Header/NavBar.tsx
+++ src/Components/Header/NavBar.tsx
@@ -1,86 +1,53 @@
 import React from "react";
 import './header.css';
 
-class NavBar extends React.Component {
+interface NavBarProps {
+    onClick: (tabID: string) => void
+}
+
+interface NavBarState {
+    selected: string
+}
+
+class NavBar extends React.Component<NavBarProps, NavBarState> {
 
     constructor(props) {
         super(props);
 
-        this.state = {home: "selected"};
-        this.handleClick = this.handleClick.bind(this)
+        this.state = {selected: "home-tab"};
     }
-
-    updateClasses(id) {
-        if (id === "docs-tab") {
-            this.setState({docs: "selected"});
-        } else {
-            this.setState({docs: ""});
-        }
-
-        if (id === "corr-tab") {
-            this.setState({corr: "selected"});
-        } else {
-            this.setState({corr: ""});
-        }
-
-        if (id === "ts-tab") {
-            this.setState({ts: "selected"});
-        } else {
-            this.setState({ts: ""});
-        }
-
-        if (id === "dl-tab") {
-            this.setState({dl: "selected"});
-        } else {
-            this.setState({dl: ""});
-        }
-
-        if (id === "vocab-tab") {
-            this.setState({vocab: "selected"});
-        } else {
-            this.setState({vocab: ""});
-        }
-
-        if (id === "home-tab") {
-            this.setState({home: "selected"});
-        } else {
-            this.setState({home: ""});
-        }
-
-        if (id === "meta-tab") {
-            this.setState({meta: "selected"});
-        } else {
-            this.setState({meta: ""});
-        }
-
-        if (id === "to-tab") {
-            this.setState({to: "selected"});
-        } else {
-            this.setState({to: ""});
-        }
-    }
-
 
     handleClick(e) {
-        var val = e.target.id;
-        this.updateClasses(val)
+        let val = e.target.id;
+        this.setState({selected: val})
         this.props.onClick(val);
     }
 
+    checkIdSelected(id: string) {
+        return this.state.selected === id ? "selected" : ""
+    }
+
+    static tabs = [
+        ['home-tab', 'Home Page'],
+        ['docs-tab', 'Topic Documents'],
+        ['corr-tab', 'Topic Correlations'],
+        ['ts-tab', 'Time Series'],
+        ['meta-tab', 'Metadata'],
+        ['to-tab', 'Topic Overview'],
+        ['dl-tab', 'Downloads'],
+        ['vocab-tab', 'Vocabulary']
+    ]
 
     render() {
         return (
             <div className="tabs">
                 <ul>
-                    <li id="home-tab" className={this.state.home} onClick={this.handleClick}>Home Page</li>
-                    <li id="docs-tab" className={this.state.docs} onClick={this.handleClick}>Topic Documents</li>
-                    <li id="corr-tab" className={this.state.corr} onClick={this.handleClick}>Topic Correlations</li>
-                    <li id="ts-tab" className={this.state.ts} onClick={this.handleClick}>Time Series</li>
-                    <li id="meta-tab" className={this.state.meta} onClick={this.handleClick}>Metadata</li>
-                    <li id="to-tab" className={this.state.to} onClick={this.handleClick}>Topic Overview</li>
-
-                    <li id="dl-tab" className={this.state.dl} onClick={this.handleClick}>Downloads</li>
-                    <li id="vocab-tab" className={this.state.vocab} onClick={this.handleClick}>Vocabulary</li>
+                    {
+                        NavBar.tabs.map(v =>
+                            <li id={v[0]} className={this.checkIdSelected(v[0])} onClick={this.handleClick.bind(this)}
+                                key={v[0]}>{v[1]}</li>
+                        )
+                    }
                 </ul>
             </div>
         );
