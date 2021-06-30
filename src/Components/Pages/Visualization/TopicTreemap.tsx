a28e4ffd0b50438707f9a10fe51c9a7c7af55845
ansiiww <iwu@hmc.edu>
2021-06-30 12:21:07 -0700

    minor changes

--- src/Components/Pages/Visualization/TopicTreemap.tsx
+++ src/Components/Pages/Visualization/TopicTreemap.tsx
@@ -5,10 +5,8 @@ import React, { ChangeEvent } from "react";
 import TreeMap, { ColorModel } from "react-d3-treemap";
 import { topNWords } from "funcs/utilityFunctions";
 import { LDAModel } from 'core'
-
 import { scaleSequential } from "d3-scale";
 import * as chromatic from "d3-scale-chromatic";
-import { relative } from 'path/posix';
 
 /**
  * @summary Component for Visualization page
@@ -55,6 +53,7 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
         return data;
     }
 
+    //handle the change of number of top words
     numChange(event: ChangeEvent<HTMLInputElement>) {
         event.preventDefault();
 
@@ -94,8 +93,8 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
                         levelsToDisplay={1}
                         nodeStyle={{paddingLeft: 10, paddingRight: 5, paddingTop:2}}
                         valueFn={n=>n.toFixed(2) + "%"}
-                        tooltipOffsetY={160}
-                        tooltipOffsetX={330}
+                        tooltipOffsetY={140}
+                        tooltipOffsetX={-40}
                         tooltipPlacement="top"
                         paddingOuter={0}
                         lightNodeBorderColor="white"
@@ -121,7 +120,7 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
                 Words with a higher probability of appearing in a given topic will represent a larger portion of the treemap's area.
                 The maximum number of words displayed by this treemap is 30. 
                 </div>
-                <div id="numWords" className="page" style={{position: "relative", left: 0, paddingBottom:0}} >
+                <div id="numWords" className="page" style={{position: "relative", left: 5, paddingBottom:0}} >
                     Number of Top Words:
                     <input
                         onChange={this.numChange.bind(this)}
