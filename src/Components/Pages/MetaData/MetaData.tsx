1dc5b1831c0ad0ebad74933fe27279c41cba879a
Simon Babb <54858379+simonbabb@users.noreply.github.com>
2021-07-08 16:03:27 -0400

    initialized MetaData.tsx

---
+++ src/Components/Pages/MetaData/MetaData.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface metaDataProps{
+}
+
+interface metaDataState{
+}
+
+
+
+export class MetaData extends React.Component<metaDataProps, metaDataState> {
+}
+
+export default MetaData;
