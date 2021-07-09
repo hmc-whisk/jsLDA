495e560c9dab7c08966a96b183185eb1069da6cb
Simon Babb <54858379+simonbabb@users.noreply.github.com>
2021-07-09 17:40:40 -0400

    fixed treemap apostrophe bug

--- src/Components/Pages/Visualization/TopicTreemap.tsx
+++ src/Components/Pages/Visualization/TopicTreemap.tsx
@@ -7,6 +7,7 @@ import {logToServer, topNWords} from "funcs/utilityFunctions";
 import { LDAModel } from 'core'
 import { scaleSequential } from "d3-scale";
 import * as chromatic from "d3-scale-chromatic";
+import { ZoomOut } from 'react-bootstrap-icons';
 
 /**
  * @summary Component for Visualization page
@@ -36,7 +37,16 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
         type wordProbPair = {
             name: string;
             value: number;
+            children: invisibleChild[];
           };
+
+          /*
+            the invisibleChild type is a stupid solution to a bug caused by having an apostrophe 
+            in a word that the treemap is displaying. If a node in the treemap has no children,
+            text with apostrophes is split and renders incorrectly. So giving each wordProbPair
+            an invisible child that doesn't show up on the treemap avoids this issue.
+          */
+          type invisibleChild = {};
           let data: { name: string; children: wordProbPair[] } = {
             name: "Top " + (this.state.numberOfTopwords ? this.state.numberOfTopwords: "") + " Word Probabilities",
             children: [],
@@ -47,7 +57,10 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
               this.props.ldaModel.selectedTopic
             );
             prob = prob*100;
-            let temp: wordProbPair = { name: word, value: prob };
+
+            let child: invisibleChild = {};
+            let temp: wordProbPair = { name: word, value: prob, children: [] };
+            temp.children.push(child);
             data.children.push(temp);
             }
         return data;
@@ -83,7 +96,7 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
         let topWordsList = topWordsString.split(" ");
         let treedata = this.topWordsProbability(topWordsList);
 
-        return <div>
+        return <div style={{pointerEvents: 'none'}}>
                     <TreeMap<typeof treedata>
                         height={500}
                         width={750}
@@ -91,7 +104,7 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
                         colorModel={ColorModel.OneEachChildren}
                         paddingInner={3}
                         levelsToDisplay={1}
-                        nodeStyle={{paddingLeft: 10, paddingRight: 5, paddingTop:2}}
+                        nodeStyle={{paddingLeft: 10, paddingRight: 5, paddingTop:2, pointerEvents: 'none'}}
                         valueFn={n=>n.toFixed(2) + "%"}
                         tooltipOffsetY={140}
                         tooltipOffsetX={-40}
@@ -99,6 +112,7 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
                         paddingOuter={0}
                         lightNodeBorderColor="white"
                         disableBreadcrumb = {true}
+                        hideNumberOfChildren = {true}
                         key={Math.random()} // force recreate element when rerender
                         // @ts-ignore
                         customD3ColorScale={scaleSequential(
@@ -124,7 +138,7 @@ export class TopicTreemap extends React.Component<topicTreemapProps, topicTreema
                     Number of Top Words:
                     <input
                         onChange={this.numChange.bind(this)}
-                        placeholder="# top Wrods"
+                        placeholder="# top Words"
                         type="number" id="numberOfBins"
                         value={this.state.numberOfTopwords}
                         max="300"
