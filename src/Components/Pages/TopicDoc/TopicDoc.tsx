d87121d964b121e99f93726a4d98e4607c4cb385
fteves22 <fteves22@cmc.edu>
2021-07-12 11:48:44 -0700

    Fixed pagination search bug

--- src/Components/Pages/TopicDoc/TopicDoc.tsx
+++ src/Components/Pages/TopicDoc/TopicDoc.tsx
@@ -171,7 +171,7 @@ export class TopicDoc extends Component<TopicDocProps, TopicDocState> {
                             alignItems: 'center',
                             marginBottom: '0.5em'
                         }}>
-                            <SearchBox model={this.props.ldaModel} search={this.search}/>
+                            <SearchBox model={this.props.ldaModel} search={this.search} changePage={this.changePage.bind(this)}/>
 
                             <div style={{display: 'flex'}}>
                                 {this.toggleMetaDataButton()}
